5ff4a03962a3a2273f2091fc4f35261e1b3479b3
Tom Colwell <100598612+tcolwell7@users.noreply.github.com>
2023-07-27 19:21:16 +0100

    update to purrr script for nested map

--- 14_purrr/14_purrr.R
+++ 14_purrr/14_purrr.R
@@ -58,6 +58,33 @@ agg_data <- read_excel("data/agg_data.xlsx")
 #' within a list or vector 
 #' 
 
+# map character vector
+trade_annual %>% map_chr(class)
+
+# map numerical
+## this is interesting way to get aggregate counts without having to aggregate or us tally()
+count <- trade_annual %>% map_dbl(n_distinct)
+count <- as.tibble(count)
+
+# create df
+df <- trade_annual %>% 
+  map_df(~(data.frame(
+                  n_distinct = n_distinct(.x),
+                  class = class(.x))
+           )
+         )
+
+df <- trade_annual %>% 
+  map_df(~(data.frame(n_distinct = n_distinct(.x),
+                        class = class(.x))),
+           .id = "col_name_variable")
+
+df <- trade_annual %>% 
+  map_df(~(data.frame(n_distinct = n_distinct(.x),
+                      class = class(.x),
+                      na.count = sum(is.na(.x), # count NAs
+                                     ))), 
+         .id = "col_name_variable")
 
 # Example: iterate through trade data set for all countries and sum total trade:
 
@@ -127,7 +154,37 @@ test_multi_input <-
     )
   
 
-## speed test vs loop ----------
+## nested map function -------
+
+# map2 functions require input lists of an even size
+# if you have an input list of length 3 and 2 - map2 won't work
+# however you can map a map2 function
+# i.e. nested map function
+# the following is a simple example using strings:
+# source: https://stackoverflow.com/questions/67542179/r-passing-different-lengthed-inputs-to-purrr-with-nested-data-structures
+
+
+# Example data: 2 different-length lists
+foo <- list(1, 2, 3)
+bar <- list("df1", "df2")
+
+# Desired output:
+out <- list(list("df1_1", "df1_2", "df1_3"),
+            list("df2_1", "df2_2", "df2_3"))
+
+
+out2 <- map(bar, ~ map(foo, function(y) paste0(.x, '_', y)))
+
+
+# pmap ---------------
+
+
+# pmap allows you add to add more than two lists and apply function
+
+
+
+
+# speed test vs loop ----------
 
 #' same process of summing df ran through loop:
 
@@ -189,6 +246,139 @@ trade_data_list <- map(country_list,function(x){trade_annual %>% filter(country=
 #' 
 
 trade_data_list2 <- map(trade_data_list,function(x){x %>% group_by(country) %>% summarise(value=sum(total))})
+
+
+### nested map example ----------
+
+# example to highlight using map to iterate through a function 
+# with different input list sizes
+# example highlighting practicle example
+# uploaded sheets (legnth 3) within a excel file
+# combined with a separate sized input
+# pulling in two files (length 2)
+
+
+sheetname = c("2.1","3.1","4.1")
+fditype = c("flows","stock","earnings")
+dir=c("outward","inward")
+
+
+out <- map(dir, ~ map2(sheetname,fditype, function(y,z) 
+  
+  read_excel("data/fdi_outward.xlsx", sheet = y, skip = 3) %>% 
+    clean_names() %>%
+    mutate(fdi_type = z) %>%
+    mutate(direc = .x)
+  
+))
+
+names(out) <- dir # rename list objects
+
+# change file names:
+
+sheetname = c("2.1","3.1","4.1")
+fditype = c("flows","stock","earnings")
+file=c("data/fdi_outward.xlsx","data/fdi_inward.xlsx")
+
+
+out <- map(file, ~ map2(sheetname,fditype, function(y,z) 
+  
+  read_excel(.x, sheet = y, skip = 3) %>% 
+    clean_names() %>%
+    mutate(fdi_type = z)
+  
+))
+
+
+# double mapping: map2 a map2 function: 
+
+sheetname = c("2.1","3.1","4.1")
+fditype = c("flows","stock","earnings")
+file=c("data/fdi_outward.xlsx","data/fdi_inward.xlsx")
+dir = c("outward","inward")
+
+out2 <- map2(file, dir, ~ map2(sheetname,fditype, function(y,z) 
+  
+  read_excel(.x, sheet = y, skip = 3) %>% 
+    clean_names() %>%
+    mutate(fdi_type = z) %>%
+    mutate(direction = .y)
+  
+))
+
+names(out2) <- dir
+
+# map to dataframe:
+
+sheetname = c("2.1","3.1","4.1")
+fditype = c("flows","stock","earnings")
+file=c("data/fdi_outward.xlsx","data/fdi_inward.xlsx")
+dir = c("outward","inward")
+
+out2df <- map2(file, dir, ~ map2_dfr(sheetname,fditype, function(y,z) 
+  
+  read_excel(
+     .x, # input from first map (file)
+     sheet = y, # input
+     skip = 3, # skip first 3 rows of excel workbook
+     col_types = "text" # ensure col types are same across every sheet
+    ) %>% 
+  clean_names() %>%
+  mutate(fdi_type = z) %>%
+  mutate(direction = .y)
+  
+))
+
+# keep/discard -----------
+
+#' keep and discard functions are powerful and simple in ammending lists
+#' 
+
+
+## simple list
+
+# create list of mixed objects
+list <- list(x1 = 1, x2 = "Second item", x3 = "Mixed list", x4 = TRUE, x5 = FALSE, x6 = NULL, x7 = NA)
+
+# remove items by name using purrr
+
+list2 <- purrr::discard(list, names(list) %in% c("x2","x6"))
+
+# remove numerical items:
+
+list2 <- purrr::keep(list, Negate(is.numeric))
+list2 <- purrr::discard(list, is.numeric)
+
+
+# remove character items
+
+list2 <- purrr::keep(list, Negate(is.character)) # remove
+list2 <- purrr::discard(list, Negate(is.character)) # using discard you cna use the same function and keep character vectors
+
+
+# remove logical elements
+
+list2 <- purrr::discard(list, is.logical)
+list2 <- purrr::keep(list, is.logical)
+
+list2 <- purrr::discard(list, isTRUE) # remove just true
+list2 <- purrr::discard(list, isFALSE) # revmoe just false
+
+
+# remove NULL
+
+list2 <- compact(list) # compact removes NULL value
+list2 <- list %>% purrr::keep( ~ !is.null(.)) # alternative
+list2 <- list %>% purrr::keep( ~ is.null(.)) # keep null values 
+
+
+
+## list of dataframes
+
+
+
+
+
 
 
 # nested data ------------
